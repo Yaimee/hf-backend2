d536f6160dcad94579a9257bef33661d39b0b42c
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-12-05 14:51:22 +0100

    Initial commit

---
+++ src/main/java/com/example/hfbackend2/HfBackend2Application.java
@@ -0,0 +1,13 @@
+package com.example.hfbackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfBackend2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HfBackend2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hfbackend2/HfBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hfbackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HfBackend2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
