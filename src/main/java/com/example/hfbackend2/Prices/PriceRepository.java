242acf0a866325b48c13031e1dbdb25243200dfa
Toby-byte <tobiasbogenielsen@gmail.com>
2022-12-08 11:31:16 +0100

    Price backend

---
+++ src/main/java/com/example/hfbackend2/Prices/PriceRepository.java
@@ -0,0 +1,8 @@
+package com.example.hfbackend2.Prices;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PriceRepository extends JpaRepository<Price,Long> {
+}
