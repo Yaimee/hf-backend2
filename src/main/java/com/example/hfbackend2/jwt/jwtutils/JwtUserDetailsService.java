2a610ee0e36f377a80bd9a964717ee3bbda68e14
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-12-08 11:24:27 +0100

    Rasmus - Ændringer i JWT

--- src/main/java/com/example/hfbackend2/jwt/jwtutils/JwtUserDetailsService.java
+++ src/main/java/com/example/hfbackend2/jwt/jwtutils/JwtUserDetailsService.java
@@ -1,25 +1,29 @@
 package com.example.hfbackend2.jwt.jwtutils;
 
 import com.example.hfbackend2.login.service.LoginService;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 //ny
+@AllArgsConstructor
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
-    LoginService service;
-    private String username = "admin";//service.findById(1L).getUsername();
-    private String password = "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6";//service.findById(1L).getPassword();
+    private LoginService service;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    //private String username = "admin";//service.findById(1L).getUsername();
+    //private String password = bCryptPasswordEncoder.encode(service.findById(1L).getPassword());
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (this.username.equals(username)) {
-            return new User(this.username,
-                    this.password,
+        if ("admin".equals(username)) {
+            return new User("admin",
+                    bCryptPasswordEncoder.encode(service.findById(1L).getPassword()),
                     new ArrayList<>());
         } else {
             throw new UsernameNotFoundException("User not found with username: " + username);
