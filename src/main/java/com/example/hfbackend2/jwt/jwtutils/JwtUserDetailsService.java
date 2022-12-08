5a636836ee084f077e3c5aa73d1b2e0a228c091f
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-12-08 12:20:25 +0100

    Rasmus - Ændringer i JWT

--- src/main/java/com/example/hfbackend2/jwt/jwtutils/JwtUserDetailsService.java
+++ src/main/java/com/example/hfbackend2/jwt/jwtutils/JwtUserDetailsService.java
@@ -12,8 +12,8 @@ import java.util.ArrayList;
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
     LoginService service;
-    private String username = service.findById(1L).getUsername();
-    private String password = service.findById(1L).getPassword();
+    private String username = "admin";//service.findById(1L).getUsername();
+    private String password = "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6";//service.findById(1L).getPassword();
 
 
     @Override
