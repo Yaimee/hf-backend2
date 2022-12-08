da51403d124626cfe72a78471db3507cae4c853c
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-12-08 03:03:03 +0100

    Rasmus - ændringer i database connection

--- src/main/java/com/example/hfbackend2/jwt/jwtutils/JwtUserDetailsService.java
+++ src/main/java/com/example/hfbackend2/jwt/jwtutils/JwtUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.example.hfbackend2.jwt.jwtutils;
 
-import com.example.hfbackend2.login.controller.LoginController;
 import com.example.hfbackend2.login.service.LoginService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,13 +11,15 @@ import java.util.ArrayList;
 //
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
-    private LoginService service;
+    LoginService service;
+    private String username = service.findById(1L).getUsername();
+    private String password = service.findById(1L).getPassword();
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (service.findById(1L).getUsername().equals(username)) {
-            return new User(service.findById(1L).getUsername(),
-                    service.findById(1L).getPassword(),
+        if (this.username.equals(username)) {
+            return new User(this.username,
+                    this.password,
                     new ArrayList<>());
         } else {
             throw new UsernameNotFoundException("User not found with username: " + username);
